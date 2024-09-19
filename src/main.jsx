dc6d956ee1d89f20a0dce8d34ce0d2674e9ebe7e
clara seijo <clasei@proton.me>
2024-09-19 16:50:41 +0200

    iteration 1 done

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { Routes, Route } from "react-router-dom";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
+
+  <Routes>
+
     <App />
-  </React.StrictMode>
+
+  </Routes>
 );
